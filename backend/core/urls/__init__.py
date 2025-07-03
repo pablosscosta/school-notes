17c9e603545e4bd26dd76923ea21d8eb8677f6e2
Pablo Costa <pablo.ufopa@gmail.com>
2025-07-02 22:09:05 -0300

    Estrutura inicial do frontend (Vue.js)

--- backend/core/urls/__init__.py
+++ backend/core/urls/__init__.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views.student_view import StudentViewSet
 from core.views.subject_view import SubjectViewSet
-from core.views.classrrom_view import ClassroomViewSet
+from core.views.classroom_view import ClassroomViewSet
 from core.views.grade_view import GradeViewSet
 
 router = DefaultRouter()
